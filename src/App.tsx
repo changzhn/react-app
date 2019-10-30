9c8a255c8e6a915a9367918aed56b3e282415fb1
changzhn <changzhenan0212@gmail.com>
2019-10-30 16:47:23 +0800

    seekbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.min.css';
+import { RangePickerProps } from 'antd/lib/date-picker/interface';
+import moment, { Moment } from 'moment';
+import SeekBar from './SeekBar';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// @ts-ignore
+window['moment'] = moment;
+
+// const opts = {
+//   locale,
+//   disabledDate: uTime.isFuture,
+//   showTime: { format: 'HH:mm' },
+//   format: 'YYYY-MM-DD HH:mm',
+//   value: [startTime, endTime] as [moment.Moment, moment.Moment],
+//   allowClear: false,
+//   ranges: GET_TIME_RANGES,
+//   onChange: event.onChange.bind(this),
+//   onOk: event.onOk.bind(this),
+//   onOpenChange: event.onOpenChange.bind(this),
+//   size: 'small' as "small" | "large" | "default" | undefined,
+//   fixIt: props.store.time.customStart, // fix it
+// } as any;
+
+export interface IState {
+  dates: [Moment, Moment];
 }
 
-export default App;
+export default class App extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props);
+    const GET_DEFAULT_END_TIME = moment();
+    const GET_DEFAULT_START_TIME = moment().add(-2, 'hour');
+
+    this.state = {
+      dates: [GET_DEFAULT_START_TIME, GET_DEFAULT_END_TIME],
+    }
+  }
+
+  public handleChange = (dates: any) => {
+    this.setState({ 
+      dates,
+    });
+  }
+
+  render() {
+    const { dates } = this.state;
+
+    const seekBarOpts = {
+      onChange: this.handleChange,
+      dates,
+    }
+    
+    const opts: RangePickerProps = {
+      format: 'YYYY-MM-DD HH:mm',
+      allowClear: false,
+      disabledDate: (currentDate: Moment | undefined) => currentDate ? currentDate.valueOf() > moment().valueOf() : false,
+      // defaultPickerValue: [moment('2018-12-12')],
+      value: dates,
+      onChange: this.handleChange,
+      renderExtraFooter: () => <SeekBar {...seekBarOpts} />
+    };
+    return (
+      <div className="App">
+        <DatePicker.RangePicker {...opts} />
+      </div>
+    )
+  }
+}
---
+++ src/SeekBar.tsx
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import { Slider, Row, Col } from 'antd';
+import './SeekBar.css';
+import { Moment } from 'moment';
+
+export interface ISeekBarProps {
+    dates: [Moment, Moment];
+    onChange: (value: [Moment, Moment]) => void;
+}
+
+const SeekBar: React.SFC<ISeekBarProps> = (props: ISeekBarProps) => {
+    const { onChange, dates: [START_TIME, END_TIME] } = props;
+
+    const startHour = START_TIME.get('hour');
+    const startMinute = START_TIME.get('minute');
+    const endHour = END_TIME.get('hour');
+    const endMinute = END_TIME.get('minute');
+
+    function handleChange(key: 'startHour' | 'startMinute' | 'endHour' | 'endMinute', value: number) {
+        const isSameDay = START_TIME.format('YYYY-MM-DD') === END_TIME.format('YYYY-MM-DD');
+        let startTime = START_TIME;
+        let endTime = END_TIME;
+
+        if (isSameDay) {
+            if (key === 'startHour') {
+                if (value > endHour) {
+                    return;
+                } else if (value === endHour && startMinute > endMinute) {
+                    START_TIME.set('minute', endMinute);
+                }
+            } else if (key === 'endHour') {
+                if (value < startHour) {
+                    return;
+                } else if (value === startHour && startMinute > endMinute) {
+                    START_TIME.set('minute', endMinute);
+                }
+            } else if (startHour === endHour) {
+                if (key === 'startMinute') {
+                    if (value > endMinute) {
+                        return;
+                    }
+                } else if (key === 'endMinute') {
+                    if (value < startMinute) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        if (key === 'startHour') {
+            startTime = START_TIME.clone().set('hour', value);
+        } else if (key === 'startMinute') {
+            startTime = START_TIME.clone().set('minute', value);
+        } else if (key === 'endHour') {
+            endTime = END_TIME.clone().set('hour', value);
+        } else if (key === 'endMinute') {
+            endTime = END_TIME.clone().set('minute', value);
+        }
+
+        if (typeof onChange === 'function') {
+            onChange([startTime, endTime]);
+        }
+    }
+
+    return (
+        <div className="seek-bar-container">
+            <div className="seek-bar-item">
+                <Row>
+                    <Col span={4}>
+                        小时：
+                    </Col>
+                    <Col span={16}>
+                        <Slider value={startHour} min={0} max={23} onChange={(value: any) => handleChange('startHour', value)} />
+                    </Col>
+                    <Col span={4}>
+                        {startHour}
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col span={4}>
+                        分：
+                    </Col>
+                    <Col span={16}>
+                        <Slider value={startMinute} min={0} max={59} onChange={(value: any) => handleChange('startMinute', value)} />
+                    </Col>
+                    <Col span={4}>
+                        {startMinute}
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="seek-bar-item">
+                <Row>
+                    <Col span={4}>
+                        小时：
+                    </Col>
+                    <Col span={16}>
+                        <Slider value={endHour} min={0} max={23} onChange={(value: any) => handleChange('endHour', value)} />
+                    </Col>
+                    <Col span={4}>
+                        {endHour}
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col span={4}>
+                        分：
+                    </Col>
+                    <Col span={16}>
+                        <Slider value={endMinute} min={0} max={59} onChange={(value: any) => handleChange('endMinute', value)} />
+                    </Col>
+                    <Col span={4}>
+                        {endMinute}
+                    </Col>
+                </Row>
+            </div>
+        </div>
+    );
+}
+
+export default SeekBar;
